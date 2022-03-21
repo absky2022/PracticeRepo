7930f9729e5266bec4bca9e971e8b0661b3ea51d
Abhay Raichand <absky2022@gmail.com>
2022-03-22 00:13:59 +0530

    Githubtest1 is updated

--- PracticeProject/src/com/nt/test/GithubTest1.java
+++ PracticeProject/src/com/nt/test/GithubTest1.java
@@ -2,12 +2,15 @@ package com.nt.test;
 
 public class GithubTest1 {
 
-	public static int sub(int a,int b) {
+	public  int sub(int a,int b) {
 		return a-b;
 	}
 	
+	
 	public static void main(String[] args) {
-		System.out.println(sub(6,5));
+		//System.out.println(sub(6,5));
+		GithubTest1 gt1 = new GithubTest1();
+		System.out.println(gt1);
 	}
 
 }
