a631cb4537644f2e5f4732557489922438ef2720
Abhay Raichand <absky2022@gmail.com>
2022-03-22 00:08:12 +0530

    Githubtest1 updated

---
+++ PracticeProject/src/com/nt/test/GithubTest1.java
@@ -0,0 +1,13 @@
+package com.nt.test;
+
+public class GithubTest1 {
+
+	public static int sub(int a,int b) {
+		return a-b;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(sub(6,5));
+	}
+
+}
